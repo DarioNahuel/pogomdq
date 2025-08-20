3f614077a72f79e6f8788d6ad91e035bc5437eb5
Dario Nahuel Rodriguez <dario.rodriguez@intive.com.ar>
2025-08-20 15:59:30 -0300

    Add event list

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,8 @@ import SocialLinks from './Components/SocialLinks';
 import SubNav from './Components/SubNav';
 import Separator from './Components/Separator';
 import Album from './Components/Album';
-import Footer from './Components/Footer'; 
+import Footer from './Components/Footer';
+import EventList from './Components/Event';
 
 function App() {
   return (
@@ -11,6 +12,8 @@ function App() {
       <SubNav />
       <Separator />
       <SocialLinks />
+      <Separator />
+      <EventList />
       <Separator />
       <Album />
       <Separator />
---
+++ src/Components/Event/Event.jsx
@@ -0,0 +1,72 @@
+import React from "react";
+
+import events from './events.json';
+import "./Event.css";
+
+import leekduck from '/leekduck.png';
+
+function getEventsThisWeek(events) {
+  const now = new Date();
+  
+  const dayOfWeek = now.getDay();
+  const diffToMonday = (dayOfWeek === 0 ? -6 : 1) - dayOfWeek;
+  const monday = new Date(now);
+  monday.setDate(now.getDate() + diffToMonday);
+  monday.setHours(0, 0, 0, 0);
+
+  const sunday = new Date(monday);
+  sunday.setDate(monday.getDate() + 6);
+  sunday.setHours(23, 59, 59, 999);
+
+  return events.filter(event => {
+    const startDate = new Date(event.start);
+    return startDate >= monday && startDate <= sunday;
+  });
+}
+
+function getDayName(fechaStr) {
+  const fecha = new Date(fechaStr);
+  const dias = ["Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado"];
+  return dias[fecha.getDay()];
+}
+
+const EventCard = ({ event }) => {
+  return (
+    <div className="event-card">
+      <div className={"event-header event-gray"}>
+        <span className="event-heading">{event.heading}</span>
+        <span className="event-day">{getDayName(event.start)}</span>
+      </div>
+
+      <div className="event-body">
+        <img src={event.image} alt={event.name} className="event-image" />
+
+        <div className="event-content">
+          <h2 className="event-title">{event.name}</h2>
+          <p className="event-date">
+            {new Date(event.start).toLocaleString()}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const EventList = () => {
+ const list = getEventsThisWeek(events);
+
+  return (
+    <div className="event-list">
+      <h1 className="event-list-title">Eventos de la semana</h1>
+      {list.map((event) => (
+        <EventCard key={event.eventID} event={event} />
+      ))}
+      <div className='event-list-footer'>
+        <span className="event-list-subtitle">Data provided by</span>
+        <img src={leekduck} alt={'data'} />
+      </div>
+    </div>
+  );
+};
+
+export default EventList;
